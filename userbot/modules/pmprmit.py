c698570b52574d228aea29ed3d43dd4bba530a9d
yusril <99790793+YusrilRNLD@users.noreply.github.com>
2022-05-09 12:10:15 +0700

    apdet pmpermit

--- userbot/modules/pmprmit.py
+++ userbot/modules/pmprmit.py
@@ -18,15 +18,13 @@ from userbot.events import ram_cmd
 from userbot.utils import edit_delete, edit_or_reply
 
 DEF_UNAPPROVED_MSG = (
-    f"╭━━━━━━━━━━━━━━━━━╮\n    🌟SELAMAT DATANG🌟\n"
-    "╰━━━━━━━━━━━━━━━━━╯\n"
-    "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n"
-    f"ANAK KONTOL ANAK NGENTOT,KALO NGECHAT MAJIKAN GUA ITU SALAM,\nHABIS ITU SABAR TUNGGU MAJIKAN GUA BALES,\nKALO GA DI BALES - BALES, LU JANGAN NYEPAM KONTOL, KAYA ANAK YATIM MAU MINTA SEMBAKO LU ANJING, APA LAGI LU NGECHAT NYA CUMA MINTA VCS, BISA GUA BLOKIR!! KALO NYEPAM JUGA TAR GUA BLOKIR!!!! TUNGGU MAJIKAN GUA NERIMA PESAN LU\n"
-    "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n"
-    "╭✠╼━━━━━━❖━━━━━━━✠╮\n"
-    "┣[• 𝐁𝐎𝐓 𝐌𝐄𝐒𝐒𝐀𝐆𝐄\n"
-    "┣[• 𝐁𝐘 ⭐️𝐑𝐀𝐌-𝐔𝐁𝐎𝐓⭐️\n"
-    "╰✠╼━━━━━━❖━━━━━━━✠╯"
+    "❏ 𝐏𝐞𝐫𝐢𝐧𝐠𝐚𝐭𝐚𝐧!!!\n"
+    "• Tuan Saya belum menyetujui anda untuk Chat.\n"
+    "• Tunggu sampai Tuan menyetujui chat anda.\n"
+    "• Jangan Spam Chat atau anda akan otomatis di blokir.\n"
+    "┌━━━━━━━━━━━━\n"
+    "├❏ 𝐏𝐞𝐬𝐚𝐧 𝐨𝐭𝐨𝐦𝐚𝐭𝐢𝐬 𝐁𝐲 𝗨𝘀𝗲𝗿𝗯𝗼𝘁\n"
+    "└━━━━━━━━━━━━\n"
 )
 
 @bot.on(events.NewMessage(incoming=True))
@@ -81,7 +79,7 @@ async def permitpm(event):
 
             if COUNT_PM[event.chat_id] > LMT_PM:
                 await event.respond(
-                    f"**Maaf mek, Lu udah di blok asisstant {owner}**"
+                    f"**Maaf Ya, Lu udah di blok asisstant Karena Tolol {owner}**"
                 )
 
                 try:
