4fc8c1018a5341585d8e013f17805ce7742f573b
yusril <99790793+YusrilRNLD@users.noreply.github.com>
2022-05-09 15:00:39 +0700

    ngetes

--- userbot/modules/Payment.py
+++ userbot/modules/Payment.py
@@ -5,6 +5,34 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 
-from userbot import CMD_HELP, BLACKLIST_CHAT, CMD_HANDLER as cmdfrom userbot.events import registerfrom userbot.utils import ram_cmd# ================= CONSTANT =================# FROM RAM-UBOT# ============================================
+from userbot import CMD_HELP, BLACKLIST_CHAT, CMD_HANDLER as cmd
 
-@register(outgoing=True, pattern='^G(?: |$)(.*)')async def _(typew): await typew.client.send_message( typew.chat_id, "**GAK ADA KEREN KERENNYA LU BEGITU NGENTOD, BAPAK LU SUJUD SUJUD DI DEPAN GUA NGENTOD GARA GARA KEBINGUNGAN GA BISA NGASIH MAKAN LU, MAKANYA DIA MINTA DI SANTUNIN PLUS DI KASIH BANSOS SAMA GUA BANGSAT!!!**", reply_to=typew.reply_to_msg_id) await typew.delete() CMD_HELP.update({ "salam": f"P or `{cmd}p`\\nUsage: Untuk Memberi salam.\\n\nL or `{cmd}l`\\nUsage: Untuk Menjawab Salam."})
+from userbot.events import register
+
+from userbot.utils import ram_cmd
+
+# ================= CONSTANT =================
+
+#                FROM RAM-UBOT
+
+# ============================================
+
+@register(outgoing=True, pattern='^G(?: |$)(.*)')
+
+async def _(typew):
+
+    await typew.client.send_message(
+
+        typew.chat_id, "**  **", reply_to=typew.reply_to_msg_id)
+
+    await typew.delete()
+
+CMD_HELP.update({
+
+    "Payments":
+
+   f"P or `{cmd}payment`\
+
+\nUsage: Untuk Menunjukkan Pembayaran.\
+
+})
